991dd29a443a7cea71339944b5fc602e3e09b704
Darshan2003 <darshanjain727@gmail.com>
2023-09-12 18:19:53 +0530

    changed env

--- src/establish_db_connection.py
+++ src/establish_db_connection.py
@@ -1,11 +1,21 @@
 from pymongo import MongoClient
-from decouple import config
 
-USERNAME = config("USER")
-PASSWORD = config("PASSWORD")
-DB_URL = config("DB_URL")
+#deployment
+import os
+
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+DB_URL = os.environ.get("DB_URL")
+
+#FOR MONGO ATLAS
+# from decouple import config
+
+# USERNAME = config("USER")
+# PASSWORD = config("PASSWORD")
+# DB_URL = config("DB_URL")
 
 # client = MongoClient(f"mongodb+srv://{USERNAME}:{PASSWORD}@{DB_URL}/?retryWrites=true&w=majority")
+
 #local connection
 client =  MongoClient("mongodb://localhost:27017/")
 database = client.RailRakshak

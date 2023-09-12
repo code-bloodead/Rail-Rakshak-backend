a46333b53323fd3cde63caea28d92ccf5e656aef
Darshan2003 <darshanjain727@gmail.com>
2023-09-12 18:24:56 +0530

    deployment fixes

--- src/establish_db_connection.py
+++ src/establish_db_connection.py
@@ -1,21 +1,14 @@
 from pymongo import MongoClient
 
-#deployment
-import os
-
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
-DB_URL = os.environ.get("DB_URL")
-
 #FOR MONGO ATLAS
-# from decouple import config
+from decouple import config
 
-# USERNAME = config("USER")
-# PASSWORD = config("PASSWORD")
-# DB_URL = config("DB_URL")
+USERNAME = config("USER")
+PASSWORD = config("PASSWORD")
+DB_URL = config("DB_URL")
 
-# client = MongoClient(f"mongodb+srv://{USERNAME}:{PASSWORD}@{DB_URL}/?retryWrites=true&w=majority")
+client = MongoClient(f"mongodb+srv://{USERNAME}:{PASSWORD}@{DB_URL}/?retryWrites=true&w=majority")
 
 #local connection
-client =  MongoClient("mongodb://localhost:27017/")
-database = client.RailRakshak
+# client =  MongoClient("mongodb://localhost:27017/")
+# database = client.RailRakshak

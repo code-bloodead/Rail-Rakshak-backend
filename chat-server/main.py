90cf0da24c8430faf6fdb386fa4aaa773b9a5f0c
Omkar Phansopkar <omkarphansopkar@gmail.com>
2023-09-24 01:26:24 +0530

    Clean up
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- chat-server/main.py
+++ chat-server/main.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
-from pyngrok import ngrok, conf
+from pyngrok import ngrok
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-from utils import generate_alphanumeric_id, contains_initiating_strings, is_report_incident_input, is_helpdesk_input, is_conclusive
+from utils import *
 
 load_dotenv()
 
@@ -40,7 +40,8 @@ def reply_whatsapp():
         msg = response.message(
             "Here are the helpline phone numbers:\nRailway Police: 1800 1113 22 \nVigilance Helpline: 0111 552 10")
     elif is_conclusive(incoming_message):
-        msg = response.message("If you need further assistance, feel free to reach out anytime.\nBye 👋")
+        msg = response.message(
+            "If you need further assistance, feel free to reach out anytime.\nBye 👋")
     else:
         incident_id = generate_alphanumeric_id()
         msg = response.message(
@@ -57,20 +58,3 @@ def ping():
 
 if __name__ == "__main__":
     app.run()
-
-# def reply_whatsapp_with_media():
-#     GOOD_BOY_URL = (
-#         "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
-#         "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-#     )
-#     try:
-#         num_media = int(request.values.get("NumMedia"))
-#     except (ValueError, TypeError):
-#         return "Invalid request: invalid or missing NumMedia parameter", 400
-#     response = MessagingResponse()
-#     if not num_media:
-#         msg = response.message("Send us an image!")
-#     else:
-#         msg = response.message("Thanks for the image. Here's one for you!")
-#         msg.media(GOOD_BOY_URL)
-#     return str(response)

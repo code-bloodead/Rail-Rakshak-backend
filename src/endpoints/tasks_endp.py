5c8e2f490b4bcbe94e35e2a992cc00a99c29e7cf
Darshan2003 <darshanjain727@gmail.com>
2023-09-23 19:09:40 +0530

    fix again

--- src/endpoints/tasks_endp.py
+++ src/endpoints/tasks_endp.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from src.models.task_model import Task, IncidentToTask
-from src.database.task_db import create_task
-from src.database.incident_db import get_incident_by_id, update_incident_status
+# from src.models.task_model import Task, IncidentToTask
+# from src.database.task_db import create_task
+# from src.database.incident_db import get_incident_by_id, update_incident_status
 
 router = APIRouter(
     prefix="/tasks",
@@ -12,40 +12,40 @@ router = APIRouter(
 # create task manually
 
 
-@router.post("/create_task")
-def create_task_manually(task: Task):
-    if task.description == "" or task.type == "" or task.deadline == "":
-        return {"ERROR": "MISSING PARAMETERS"}
+# @router.post("/create_task")
+# def create_task_manually(task: Task):
+#     if task.description == "" or task.type == "" or task.deadline == "":
+#         return {"ERROR": "MISSING PARAMETERS"}
 
-    if task.assigned_to != []:
-        task.status = "Assigned"
+#     if task.assigned_to != []:
+#         task.status = "Assigned"
 
-    result = create_task(task)
-    return result
+#     result = create_task(task)
+#     return result
 
-# convert incident to task
+# # convert incident to task
 
 
-@router.post("/convert_incident_to_task")
-def convert_incident_to_task(incedent_to_task: IncidentToTask):
-    if incedent_to_task.incident_id == "" or incedent_to_task.deadline == "":
-        return {"ERROR": "MISSING PARAMETERS"}
+# @router.post("/convert_incident_to_task")
+# def convert_incident_to_task(incedent_to_task: IncidentToTask):
+#     if incedent_to_task.incident_id == "" or incedent_to_task.deadline == "":
+#         return {"ERROR": "MISSING PARAMETERS"}
 
-    incident = get_incident_by_id(incedent_to_task.incident_id)
+#     incident = get_incident_by_id(incedent_to_task.incident_id)
 
-    task = Task(
-        description=incident.description, 
-        image=incident.image,
-        deadline=incedent_to_task.deadline, 
-        type=incident.type,
-        assc_incident=incedent_to_task.incident_id
-    )
+#     task = Task(
+#         description=incident.description, 
+#         image=incident.image,
+#         deadline=incedent_to_task.deadline, 
+#         type=incident.type,
+#         assc_incident=incedent_to_task.incident_id
+#     )
 
-    if incedent_to_task.assigned_to == []:
-        task.status = "Assigned"
+#     if incedent_to_task.assigned_to == []:
+#         task.status = "Assigned"
 
-    result = create_task(task)
+#     result = create_task(task)
 
-    update_incident_status(incedent_to_task.incident_id, "In Progress")
+#     update_incident_status(incedent_to_task.incident_id, "In Progress")
 
-    return result
+#     return result

b3d7c0b6cced90d3dc5db2b3ccc3f1747dce9dfa
Darshan2003 <darshanjain727@gmail.com>
2023-09-26 23:06:21 +0530

    upload image

--- src/endpoints/incidents_endp.py
+++ src/endpoints/incidents_endp.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-# from fastapi import UploadFile, Form
+from fastapi import UploadFile, Form
 from src.models.incidents_model import Incidents
 from src.database.incident_db import (create_incident, fetch_all_incidents, fetch_incidents_by_dept_and_station, delete_incident_by_id)
 from src.config import AWS_KEY, SECRET_KEY_AWS, S3_BUCKET_NAME
@@ -41,27 +41,27 @@ def new_incident(incident: Incidents):
         print(e)
         return {"ERROR":"SOME ERROR OCCURRED"}
 
-# @router.post("/user_incident")
-# def create_incident_by_user(image: UploadFile, title: str = Form(...), description: str = Form(...), type: str = Form(...), station_name: str = Form(...), location: str = Form(...), source: str = Form(...)):
-#     try:
-#         incident = Incidents(title=title, description=description, type=type, station_name=station_name, location=location, source=source)
+@router.post("/user_incident")
+def create_incident_by_user(image: UploadFile, title: str = Form(...), description: str = Form(...), type: str = Form(...), station_name: str = Form(...), location: str = Form(...), source: str = Form(...)):
+    try:
+        incident = Incidents(title=title, description=description, type=type, station_name=station_name, location=location, source=source)
 
-#         filename = image.filename.replace(" ","")
-#         img_extension = filename.split(".")[1]
+        filename = image.filename.replace(" ","")
+        img_extension = filename.split(".")[1]
             
-#         if img_extension not in ["png", "jpg","jpeg"]:
-#             return {"ERROR":"INVALID IMAGE FORMAT"}
+        if img_extension not in ["png", "jpg","jpeg"]:
+            return {"ERROR":"INVALID IMAGE FORMAT"}
 
-#         uname = str(filename.split(".")[0] + generateID() + "."+ img_extension)
-#         bucket.upload_fileobj(image.file, uname)
-#         url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{uname}"
-#         incident.image = url
+        uname = str(filename.split(".")[0] + generateID() + "."+ img_extension)
+        bucket.upload_fileobj(image.file, uname)
+        url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{uname}"
+        incident.image = url
 
-#         result = create_incident(incident)
-#         return result
-#     except Exception as e:
-#         print(e)
-#         return {"ERROR":"SOME ERROR OCCURRED"}
+        result = create_incident(incident)
+        return result
+    except Exception as e:
+        print(e)
+        return {"ERROR":"SOME ERROR OCCURRED"}
 
 ## get all incidents
 @router.get("/all_incidents")
